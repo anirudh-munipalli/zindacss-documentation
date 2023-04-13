1945bf089536a6c9d40a0b7865d5197ae1550bc5
anirudh-munipalli <74961859+anirudh-munipalli@users.noreply.github.com>
2023-04-13 21:09:04 +0530

    Add files via upload

---
+++ include/index.js
@@ -0,0 +1,5 @@
+var link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "https://anirudh-munipalli.github.io/zindacss-documentation/CSSCompiled/ZindaCSS.css"
+
+document.head.appendChild(link)
